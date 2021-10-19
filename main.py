4c2551d44208aafa09be3c6e21e9e4e92f0bac77
Shaon <30725033+shasut@users.noreply.github.com>
2021-10-19 21:18:49 +0000

    test case changes

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 
 import stn_mnist, stn_stl10, stn_svhn, stn_deformConv_mnist#, vit_mnist
+import warnings
+warnings.filterwarnings('ignore')
+
 
 if __name__ == "__main__":
     """
@@ -16,11 +19,11 @@ if __name__ == "__main__":
     # stn_mnist.stn_base(train_epoch=EPOCH, use_stn=True, use_coord_conv=True, verbose=VERBOSE, flag="with_stn_with_coord")
     #
     # print("--"*20)
-    print("\nSTN base svhn with stn without coordConv")
-    stn_svhn.stn_color_svhn(train_epoch=EPOCH, use_stn=True, use_coord_conv=False, verbose=VERBOSE, flag="with_stn_without_coord")
-    print("--"*20)
-    print("\nSTN base svhn with stn with coordConv")
-    stn_svhn.stn_color_svhn(train_epoch=EPOCH, use_stn=True, use_coord_conv=True, verbose=VERBOSE, flag="with_stn_with_coord")
+    # print("\nSTN base svhn with stn without coordConv")
+    # stn_svhn.stn_color_svhn(train_epoch=EPOCH, use_stn=True, use_coord_conv=False, verbose=VERBOSE, flag="with_stn_without_coord")
+    # print("--"*20)
+    # print("\nSTN base svhn with stn with coordConv")
+    # stn_svhn.stn_color_svhn(train_epoch=EPOCH, use_stn=True, use_coord_conv=True, verbose=VERBOSE, flag="with_stn_with_coord")
 
     # print("--"*20)
     # print("\nSTN base stl10 with stn with coordConv")
@@ -30,11 +33,11 @@ if __name__ == "__main__":
     # stn_stl10.stn_color_stl(train_epoch=EPOCH, use_stn=True, use_coord_conv=False, verbose=VERBOSE, flag="with_stn_with_coord")
     #
     # print("--"*20)
-    # print("\nSTN base mnist with stn with coord")
-    # stn_deformConv_mnist.stn_deform_conv(train_epoch=EPOCH, use_stn=True, use_coord_conv=False, verbose=VERBOSE, flag="with_stn")
+    print("\nSTN base mnist with stn with coord")
+    stn_deformConv_mnist.stn_deform_conv(train_epoch=EPOCH, use_stn=True, use_coord_conv=False, verbose=VERBOSE, flag="with_stn")
 
-    # print("--"*20)
-    # print("\nSTN base mnist with stn with coord")
-    # vit_mnist.vit_base(train_epoch=EPOCH, use_stn=True, use_coord_conv=False, verbose=VERBOSE, flag="with_stn")
+    print("--"*20)
+    print("\nSTN base mnist with stn with coord")
+    vit_mnist.vit_base(train_epoch=EPOCH, use_stn=True, use_coord_conv=False, verbose=VERBOSE, flag="with_stn")
 
-    pass
+    # pass
